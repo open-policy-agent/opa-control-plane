af06b8cc82fefd6b78099a812a3c87ba4560b3ff
Stephan Renatus <stephan.renatus@gmail.com>
2025-10-13 09:22:33 +0200

    builder: support "mounts", i.e. rewrites of rego and data in sources
    This change allows users to drive certain bundle data and policy rewrites
    from the configuration.
    
    We're calling it "mounts", and they come with (optional) sub and prefix
    settings. A "sub" will select a subtree of `data` and put it into a new
    "prefix". E.g. the following mount,
    
        sub: data.library
        prefix: data.imported.lib.v1
    
    would change all `package` statements of the rego files within this
    source and sources it depends on. A package like "data.library.authz"
    would be rewritten to "data.imported.lib.v1.authz". All references are
    adjusted.
    
    Mounts are aggrated, and applied in order, if the source that you depend
    on has a mount configuration, too.
    
    Subtrees of data that are not selected by a "sub" are carried along.
    It is possible to prefix everything that comes with a source by using
    sub "data" (and prefix "data.source1" for example).
    
    Fixes #30.
    
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- internal/database/database.go
+++ internal/database/database.go
@@ -581,6 +581,7 @@ func (d *Database) ListBundles(ctx context.Context, principal string, opts ListO
         secrets.name AS secret_name,
         secrets.value AS secret_value,
 		bundles_requirements.source_name AS req_src,
+		bundles_requirements.mounts AS req_mounts,
 		bundles_requirements.gitcommit AS req_commit
     FROM
         bundles
@@ -622,7 +623,7 @@ func (d *Database) ListBundles(ctx context.Context, principal string, opts ListO
 			filepath                                   *string // File system storage
 			excluded                                   *string
 			secretName, secretValue                    *string
-			reqSrc, reqCommit                          *string
+			reqSrc, reqMounts, reqCommit               *string
 		}
 		bundleMap := make(map[string]*config.Bundle)
 		idMap := make(map[string]int64)
@@ -635,7 +636,8 @@ func (d *Database) ListBundles(ctx context.Context, principal string, opts ListO
 				&row.gcpProject, &row.gcpObject, // GCP
 				&row.azureAccountURL, &row.azureContainer, &row.azurePath, // Azure
 				&row.filepath,
-				&row.excluded, &row.secretName, &row.secretValue, &row.reqSrc, &row.reqCommit); err != nil {
+				&row.excluded, &row.secretName, &row.secretValue,
+				&row.reqSrc, &row.reqMounts, &row.reqCommit); err != nil {
 				return nil, "", err
 			}
 
@@ -712,7 +714,16 @@ func (d *Database) ListBundles(ctx context.Context, principal string, opts ListO
 			}
 
 			if row.reqSrc != nil {
-				bundle.Requirements = append(bundle.Requirements, config.Requirement{Source: row.reqSrc, Git: config.GitRequirement{Commit: row.reqCommit}})
+				req := config.Requirement{
+					Source: row.reqSrc,
+					Git:    config.GitRequirement{Commit: row.reqCommit},
+				}
+				if row.reqMounts != nil {
+					if err := json.Unmarshal([]byte(*row.reqMounts), &req.Mounts); err != nil {
+						return nil, "", fmt.Errorf("failed to unmarshal mounts for %q/%q: %w", bundle.Name, *req.Source, err)
+					}
+				}
+				bundle.Requirements = append(bundle.Requirements, req)
 			}
 
 			if row.id > lastId {
@@ -1345,9 +1356,12 @@ func (d *Database) UpsertBundle(ctx context.Context, principal string, bundle *c
 		sources := []string{}
 		for _, req := range bundle.Requirements {
 			if req.Source != nil {
-				// TODO: add support for mounts on requirements; currently that is only used internally for stacks.
-				if err := d.upsert(ctx, tx, "bundles_requirements", []string{"bundle_name", "source_name", "gitcommit"}, []string{"bundle_name", "source_name"},
-					bundle.Name, req.Source, req.Git.Commit); err != nil {
+				mounts, err := json.Marshal(req.Mounts)
+				if err != nil {
+					return err
+				}
+				if err := d.upsert(ctx, tx, "bundles_requirements", []string{"bundle_name", "source_name", "gitcommit", "mounts"}, []string{"bundle_name", "source_name"},
+					bundle.Name, req.Source, req.Git.Commit, string(mounts)); err != nil {
 					return err
 				}
 				sources = append(sources, *req.Source)
