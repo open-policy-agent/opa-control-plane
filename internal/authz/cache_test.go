267f82ee5e878ba33f920a078611c0d74c6ec9d3
Ashutosh Narkar <anarkar4387@gmail.com>
2026-01-27 15:59:44 -0800

    Add Pluggable Authorization Model for Data Layer
    Currently, the authorization logic within OCP is tightly
    coupled with the `database` package. This change adds support
    for a pluggable authorization model in order to decouple
    the data layer from authorization logic.
    
    Fixes: #235
    
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

--- internal/authz/cache_test.go
+++ internal/authz/cache_test.go
@@ -2,48 +2,50 @@ package authz
 
 import (
 	"testing"
+
+	"github.com/open-policy-agent/opa-control-plane/pkg/authz"
 )
 
 func TestCache(t *testing.T) {
 	// reset cache
 	partialCache = newCache(cacheSize)
 
-	expr1 := sqlExprIsNotNull{Column: ColumnRef{Table: "table1", Column: "id1"}}
-	expr2 := sqlExprIsNotNull{Column: ColumnRef{Table: "table2", Column: "id2"}}
-	expr3 := sqlExprIsNotNull{Column: ColumnRef{Table: "table3", Column: "id3"}}
-	expr4 := sqlExprIsNotNull{Column: ColumnRef{Table: "table4", Column: "id4"}}
-	expr5 := sqlExprIsNotNull{Column: ColumnRef{Table: "table5", Column: "id5"}}
-	expr6 := sqlExprIsNotNull{Column: ColumnRef{Table: "table6", Column: "id6"}}
+	expr1 := sqlExprIsNotNull{Column: authz.SQLColumnRef{Table: "table1", Column: "id1"}}
+	expr2 := sqlExprIsNotNull{Column: authz.SQLColumnRef{Table: "table2", Column: "id2"}}
+	expr3 := sqlExprIsNotNull{Column: authz.SQLColumnRef{Table: "table3", Column: "id3"}}
+	expr4 := sqlExprIsNotNull{Column: authz.SQLColumnRef{Table: "table4", Column: "id4"}}
+	expr5 := sqlExprIsNotNull{Column: authz.SQLColumnRef{Table: "table5", Column: "id5"}}
+	expr6 := sqlExprIsNotNull{Column: authz.SQLColumnRef{Table: "table6", Column: "id6"}}
 
-	columns1 := map[string]ColumnRef{"id1": {Table: "tablea", Column: "ida"}}
-	columns2 := map[string]ColumnRef{"id1": {Table: "tablea", Column: "ida"}, "id2": {Table: "tableb", Column: "idb"}}
+	columns1 := map[string]authz.SQLColumnRef{"id1": {Table: "tablea", Column: "ida"}}
+	columns2 := map[string]authz.SQLColumnRef{"id1": {Table: "tablea", Column: "ida"}, "id2": {Table: "tableb", Column: "idb"}}
 
 	testCases := []struct {
 		name    string
 		key     Access
-		columns map[string]ColumnRef
-		value   Expr
+		columns map[string]authz.SQLColumnRef
+		value   authz.Expr
 		err     error
 		hit     bool
 	}{
-		{name: "miss_1", key: Access{Principal: "p0", Resource: "r0", Permission: "perm0"}, value: expr1, hit: false},
-		{name: "miss_2", key: Access{Principal: "p1", Resource: "r0", Permission: "perm0"}, value: expr2, hit: false},
-		{name: "miss_3", key: Access{Principal: "p1", Resource: "r1", Permission: "perm0"}, value: expr3, hit: false},
-		{name: "miss_4", key: Access{Principal: "p1", Resource: "r1", Permission: "perm1"}, value: expr4, hit: false},
-		{name: "miss_5", key: Access{Principal: "p1", Resource: "r1", Permission: "perm1"}, columns: columns1, value: expr5, hit: false},
-		{name: "miss_6", key: Access{Principal: "p1", Resource: "r1", Permission: "perm1"}, columns: columns2, value: expr6, hit: false},
-		{name: "hit_1", key: Access{Principal: "p0", Resource: "r0", Permission: "perm0"}, value: expr1, hit: true},
-		{name: "hit_2", key: Access{Principal: "p1", Resource: "r0", Permission: "perm0"}, value: expr2, hit: true},
-		{name: "hit_3", key: Access{Principal: "p1", Resource: "r1", Permission: "perm0"}, value: expr3, hit: true},
-		{name: "hit_4", key: Access{Principal: "p1", Resource: "r1", Permission: "perm1"}, value: expr4, hit: true},
-		{name: "hit_5", key: Access{Principal: "p1", Resource: "r1", Permission: "perm1"}, columns: columns1, value: expr5, hit: true},
-		{name: "hit_6", key: Access{Principal: "p1", Resource: "r1", Permission: "perm1"}, columns: columns2, value: expr6, hit: true},
+		{name: "miss_1", key: Access{principal: "p0", resource: "r0", permission: "perm0"}, value: expr1, hit: false},
+		{name: "miss_2", key: Access{principal: "p1", resource: "r0", permission: "perm0"}, value: expr2, hit: false},
+		{name: "miss_3", key: Access{principal: "p1", resource: "r1", permission: "perm0"}, value: expr3, hit: false},
+		{name: "miss_4", key: Access{principal: "p1", resource: "r1", permission: "perm1"}, value: expr4, hit: false},
+		{name: "miss_5", key: Access{principal: "p1", resource: "r1", permission: "perm1"}, columns: columns1, value: expr5, hit: false},
+		{name: "miss_6", key: Access{principal: "p1", resource: "r1", permission: "perm1"}, columns: columns2, value: expr6, hit: false},
+		{name: "hit_1", key: Access{principal: "p0", resource: "r0", permission: "perm0"}, value: expr1, hit: true},
+		{name: "hit_2", key: Access{principal: "p1", resource: "r0", permission: "perm0"}, value: expr2, hit: true},
+		{name: "hit_3", key: Access{principal: "p1", resource: "r1", permission: "perm0"}, value: expr3, hit: true},
+		{name: "hit_4", key: Access{principal: "p1", resource: "r1", permission: "perm1"}, value: expr4, hit: true},
+		{name: "hit_5", key: Access{principal: "p1", resource: "r1", permission: "perm1"}, columns: columns1, value: expr5, hit: true},
+		{name: "hit_6", key: Access{principal: "p1", resource: "r1", permission: "perm1"}, columns: columns2, value: expr6, hit: true},
 	}
 
 	for _, tc := range testCases {
 		hit := true
 		t.Run(tc.name, func(t *testing.T) {
-			value, err := partialCache.Get(tc.key, tc.columns, func() (Expr, error) {
+			value, err := partialCache.Get(tc.key, tc.columns, func() (authz.Expr, error) {
 				hit = false
 				return tc.value, tc.err
 			})
