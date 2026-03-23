e0a6c5c0ac3f08579c00a5cf7b02fe350406c6f3
Stephan Renatus <stephan.renatus@gmail.com>
2026-03-23 20:11:27 +0100

    examples/docker: swap out localstack for s3proxy
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- examples/docker/init-s3.sh
+++ examples/docker/init-s3.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-# Wait for LocalStack to be ready (check for S3 availability using awslocal)
-until awslocal --endpoint-url=http://localstack:4566 s3 ls > /dev/null 2>&1; do
-  echo "Waiting for LocalStack S3 to start..."
+# Wait for s3proxy to be ready
+until curl -sf http://s3proxy:80/ > /dev/null 2>&1; do
+  echo "Waiting for s3proxy to start..."
   sleep 1
 done
 
-echo "LocalStack S3 is ready. Creating bucket..."
-awslocal --endpoint-url=http://localstack:4566 s3 mb s3://bundles
+echo "s3proxy is ready. Creating bucket..."
+curl -sf --request PUT http://s3proxy:80/bundles
+echo "done"
 
 sleep infinity
